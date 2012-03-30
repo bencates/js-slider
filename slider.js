1e44f7e67a81256dd9a8bffd5538a818c715a885
Ben Cates <ben.cates@gmail.com>
2012-03-30 15:46:39 -0400

    Moved event handler registration into code

--- slider.js
+++ slider.js
@@ -1,18 +1,41 @@
 "use strict";
 
-var current = 0;
-
-function slidePrev () {
-    slideTo(current-1);
-}
-
-function slideNext () {
-    slideTo(current+1);
-}
-
-function slideTo (target) {
-    if (target >= 0 && target < 3) {
-        current = target;
-        document.getElementById('slider').scrollLeft = 600 * current;
+window.Slider = {
+    current : 0,
+    
+    prev : function () {
+        this.moveTo(this.current-1);
+    },
+    
+    next : function () {
+        this.moveTo(this.current+1);
+    },
+    
+    moveTo : function (target) {
+        if (target >= 0 && target < 3) {
+            this.current = target;
+            document.getElementById('slider').scrollLeft = 600 * this.current;
+        }
     }
-}
+};
+
+window.addEventListener('DOMContentLoaded', function() {
+    var buttons, i, data;
+    
+    buttons = document.getElementById('controls').getElementsByTagName('button');
+    
+    for (i = 0; i < buttons.length; i++) {
+        data = buttons[i].dataset || {};
+        
+        if (data.sliderTarget) {
+            data.sliderAction = 'moveTo';
+        }
+        
+        if (data.sliderAction) {
+            buttons[i].addEventListener('click', function(event) {
+                var data = event.currentTarget.dataset;
+                Slider[data.sliderAction](parseInt(data.sliderTarget, 10));
+            });
+        }
+    }
+});
