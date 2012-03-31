7b1fb624913e9f01582e757fa03b684f9f9bb897
Ben Cates <ben.cates@gmail.com>
2012-03-30 22:43:06 -0400

    Moved Slider into a 'class'

--- slider.js
+++ slider.js
@@ -1,29 +1,26 @@
 "use strict";
 
-window.Slider = {
-    current : 0,
+window.Slider = function(slider, controls, options) {
+    var buttons, i, data, self = this;
     
-    prev : function () {
-        this.moveTo(this.current-1);
-    },
+    this.slider = document.getElementById(slider || 'slider');
+    this.controls = document.getElementById(controls || 'controls');
+    this.slides = this.slider.getElementsByClassName('slide');
     
-    next : function () {
-        this.moveTo(this.current+1);
-    },
+    this.options = {
+        width   : 600,
+        initial : 0
+    };
     
-    moveTo : function (target) {
-        if (target >= 0 && target < 3) {
-            this.current = target;
-            document.getElementById('slider').scrollLeft = 600 * this.current;
-        }
+    for (key in (options || {})) {
+        this.options[key] = options[key];
     }
-};
-
-window.addEventListener('DOMContentLoaded', function() {
-    var buttons, i, data;
     
-    buttons = document.getElementById('controls').getElementsByTagName('button');
+    if (!this.options.size) {
+        this.options.size = this.slides.length;
+    }
     
+    buttons = this.controls.getElementsByTagName('button');
     for (i = 0; i < buttons.length; i++) {
         data = buttons[i].dataset || {};
         
@@ -34,8 +31,31 @@ window.addEventListener('DOMContentLoaded', function() {
         if (data.sliderAction) {
             buttons[i].addEventListener('click', function(event) {
                 var data = event.currentTarget.dataset;
-                Slider[data.sliderAction](parseInt(data.sliderTarget, 10));
+                self[data.sliderAction](+data.sliderTarget);
             });
         }
     }
+    
+    this.moveTo(this.options.initial);
+};
+
+window.Slider.prototype = {
+    prev : function () {
+        this.moveTo(this.current-1);
+    },
+    
+    next : function () {
+        this.moveTo(this.current+1);
+    },
+    
+    moveTo : function (target) {
+        if (target >= 0 && target < this.options.size) {
+            this.current = target;
+            this.slider.scrollLeft = this.options.width * this.current;
+        }
+    }
+};
+
+window.addEventListener('DOMContentLoaded', function() {
+    var slider = new Slider();
 });
